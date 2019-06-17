6875584164437d63b36a5091f5685ce202bc1091
Aleksandar Evstatiev <Aleksandar_Evstatiev@epam.com>
2019-06-17 10:26:45 +0300

    common methods

---
+++ src/main/java/common_methods/equals/Color.java
@@ -0,0 +1,5 @@
+package common_methods.equals;
+
+public enum Color {
+    RED, ORANGE, YELLOW, GREEN, BLUE, INDIGO, VIOLET
+}
---
+++ src/main/java/common_methods/equals/composition/ColorPoint.java
@@ -0,0 +1,33 @@
+package common_methods.equals.composition;
+
+import common_methods.equals.Color;
+import common_methods.equals.Point;
+
+import java.util.Objects;
+
+public class ColorPoint {
+    private final Point point;
+    private final Color color;
+
+    public ColorPoint(int x, int y, Color color) {
+        point = new Point(x, y);
+        this.color = Objects.requireNonNull(color);
+    }
+
+    public Point asPoint() {
+        return point;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof ColorPoint))
+            return false;
+        ColorPoint cp = (ColorPoint) o;
+        return cp.point.equals(point) && cp.color.equals(color);
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * point.hashCode() + color.hashCode();
+    }
+}
